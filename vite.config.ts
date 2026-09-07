a7580e1d863be405dd2664feedf1120a1e6af252
Cedric Chapeaucou <ced@Cedrics-MacBook.local>
2026-09-07 13:22:27 -0400

    Deploy frontend with GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/3d-multiplayer-mini-world/" : "/",
   server: {
     host: "localhost",
     port: 5173,
